0144aed0adc6fdc4b9f95a8318bc4b5fce973ce2
Jorge C. Leitão <jorgecarleitao@gmail.com>
2013-11-19 17:39:59 +0100

    Added more documentation to example.

--- tools/example.py
+++ tools/example.py
@@ -1,14 +1,18 @@
+# these first two lines are used to setup a minimal Django environment
 import set_up
 set_up.set_up_django_environment()
+# From this point on, we are ready to use Django for accessing the remote database.
+
 from contracts import models
-
-all_contracts = models.Contract.objects.all()
-
-number_of_contracts = all_contracts.count()
-
-print number_of_contracts
-
 from django.db.models import Sum
 
+# this query asks for all contracts in the database
+all_contracts = models.Contract.objects.all()
+
+# this query counts the previous query
+number_of_contracts = all_contracts.count()
+print number_of_contracts
+
+# this query sums the prices of all contracts.
 total_price = all_contracts.aggregate(sum=Sum('price'))['sum']
 print total_price
