dd8398a2a3eb785032d5b8abc328edfcb68ec08e
Jorge C. Leitão <jorgecarleitao@gmail.com>
2013-12-13 14:47:21 +0100

    Added new analysis: percentage of direct contracts and tender contracts.

--- contracts/analysis/__init__.py
+++ contracts/analysis/__init__.py
@@ -28,10 +28,10 @@ def get_ranking():
     (Used to filter municipalities that appear twice in the official database)
     4. We order them by decreasing average depth
     """
-    return models.Entity.objects\
-        .filter(name__startswith=u'Município')\
-        .annotate(avg_depth=Avg('contracts_made__category__depth'))\
-        .filter(avg_depth__gt=0)\
+    return models.Entity.objects \
+        .filter(name__startswith=u'Município') \
+        .annotate(avg_depth=Avg('contracts_made__category__depth')) \
+        .filter(avg_depth__gt=0) \
         .order_by('-avg_depth')
 
 
@@ -52,3 +52,39 @@ def get_contracts_macro_statistics():
             'year_count': year_price['count'],
             'month_sum': month_price['sum'],
             'month_count': month_price['count']}
+
+
+def get_procedure_types_time_series():
+    import calendar
+    import datetime
+    def add_months(sourcedate,months):
+        month = sourcedate.month - 1 + months
+        year = sourcedate.year + month / 12
+        month = month % 12 + 1
+        day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+        return datetime.date(year, month, day)
+
+    min_date = datetime.date(2010, 1, 1)
+    end_date = datetime.date(date.today().year, date.today().month, 1)
+
+    data = []
+    for i in range(100):
+        max_date = add_months(min_date, 1)
+        contracts = models.Contract.objects.filter(added_date__gte=min_date,
+                                                   added_date__lt=max_date)
+
+        count = contracts.count()
+        if count == 0:
+            break
+
+        entry = {'from': min_date,
+                 'to': max_date,
+                 'direct': contracts.filter(procedure_type_id=2).count()*1./count,
+                 'tender': contracts.filter(procedure_type_id=3).count()*1./count
+        }
+        data.append(entry)
+        min_date = max_date
+        if min_date == end_date:
+            break
+
+    return data
