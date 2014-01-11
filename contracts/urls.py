664b0611fc5857e48ce68f8797561a918591ed8d
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-01-11 17:39:37 +0100

    Improved url name of new analysis.

--- contracts/urls.py
+++ contracts/urls.py
@@ -34,6 +34,6 @@ urlpatterns = patterns('',
                        url(r'^%s/data$' % _('municipalities-delta-time'), views_data.municipalities_delta_time_json, name='municipalities_delta_time_json'),
                        url(r'^%s/data/histogram$' % _('municipalities-delta-time'), views_data.municipalities_delta_time_histogram_json, name='municipalities_delta_time_histogram_json'),
 
-                       url(r'^%s$' % _('when-do-portuguese-municipalities-contract'), views_analysis.municipalities_contracts_time_series, name='municipalities_contracts_time_series'),
-                       url(r'^%s/data$' % _('when-do-portuguese-municipalities-contract'), views_data.municipalities_contracts_time_series_json, name='municipalities_contracts_time_series_json')
+                       url(r'^%s$' % _('when-do-portuguese-municipalities-contract-most'), views_analysis.municipalities_contracts_time_series, name='municipalities_contracts_time_series'),
+                       url(r'^%s/data$' % _('when-do-portuguese-municipalities-contract-most'), views_data.municipalities_contracts_time_series_json, name='municipalities_contracts_time_series_json')
 )
