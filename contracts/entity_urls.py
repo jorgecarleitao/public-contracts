ab231e52da3d18e4599b7de50375ffb33360639b
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-03-16 14:12:59 +0100

    Added tab with tenders of entity.

--- contracts/entity_urls.py
+++ contracts/entity_urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
                        url(r'(\d+)$', entity_views.main_view, name='entity_canonical'),
                        url(r'(\d+)/%s$' % _('contracts'), entity_views.contracts, name='entity_contracts'),
                        url(r'(\d+)/%s$' % _('costumers'), entity_views.costumers, name='entity_costumers'),
+                       url(r'(\d+)/%s$' % _('tenders'), entity_views.tenders, name='entity_tenders'),
                        url(r'(\d+)/%s/rs-s$' % _('contracts'), feed.EntityFeed()),
                        url(r'(\d+)/(\w+)', entity_views.main_view, name='entity'),
                        )
