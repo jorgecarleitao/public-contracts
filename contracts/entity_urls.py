ec66a7049a3f516d4b3845af213791ec638e6b42
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-03-16 16:41:02 +0100

    Fixed rss of entities contracts and added one for tenders.

--- contracts/entity_urls.py
+++ contracts/entity_urls.py
@@ -7,8 +7,12 @@ import feed
 urlpatterns = patterns('',
                        url(r'(\d+)$', entity_views.main_view, name='entity_canonical'),
                        url(r'(\d+)/%s$' % _('contracts'), entity_views.contracts, name='entity_contracts'),
+                       url(r'(\d+)/%s/rss$' % _('contracts'), feed.EntityContractsFeed()),
+
                        url(r'(\d+)/%s$' % _('costumers'), entity_views.costumers, name='entity_costumers'),
+
                        url(r'(\d+)/%s$' % _('tenders'), entity_views.tenders, name='entity_tenders'),
-                       url(r'(\d+)/%s/rs-s$' % _('contracts'), feed.EntityFeed()),
+                       url(r'(\d+)/%s/rss$' % _('tenders'), feed.EntityTendersFeed()),
+
                        url(r'(\d+)/(\w+)', entity_views.main_view, name='entity'),
                        )
