a9944a885caac2be1cc6a41bf3af48ed2dd793ee
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-04-08 19:47:40 +0200

    Schedule now correctly loads latest documents.

--- law/tools/schedule.py
+++ law/tools/schedule.py
@@ -7,12 +7,9 @@ import set_up
 set_up.set_up_django_environment('settings')
 
 from law.crawler import FirstSeriesCrawler
-from law.analysis.analysis import get_documents_time_series
 
 crawler = FirstSeriesCrawler()
 
-#crawler.extract_law_types()
-#crawler.retrieve_all_summaries()
+crawler.extract_law_types()
+crawler.retrieve_all_summaries()
 crawler.save_all_summaries()
-
-#get_documents_time_series()
