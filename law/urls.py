fbc4a820d470944ba5772b4a95bba96db0d27431
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-04-08 20:09:05 +0200

    Fixed typo in urls.py of law.

--- law/urls.py
+++ law/urls.py
@@ -6,5 +6,5 @@ import views
 urlpatterns = patterns('',
                        url(r'^/%s$' % _('home'), views.home, name='law_home'),
                        url(r'^$', views.law_list, name='law_law_list'),
-                       url(r'^%s$' % _('types'), views.types_list, name='law_types_list'),
+                       url(r'^/%s$' % _('types'), views.types_list, name='law_types_list'),
 )
