a482fe62a5d999051a564762ec13423c27ab604c
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-04-13 22:37:13 +0200

    Added app names to template context.

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import resolve
 from main.domain import SITE_NAME, SITE_DOMAIN
 
 
@@ -5,4 +6,6 @@ def site(request):
     """
     Returns context variables of the website
     """
-    return {'SITE_NAME': SITE_NAME, 'SITE_DOMAIN': SITE_DOMAIN}
+    return {'SITE_NAME': SITE_NAME,
+            'SITE_DOMAIN': SITE_DOMAIN,
+            'main_navigation_tab': resolve(request.path).app_name}
