cd77d389a01428eded3f9ef2bdd68042297cb7bd
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-05-09 15:59:37 +0200

    Fixed old integer / to // (Python 3).

--- contracts/analysis/analysis.py
+++ contracts/analysis/analysis.py
@@ -13,7 +13,7 @@ from contracts import models
 
 def add_months(sourcedate, months):
     month = sourcedate.month - 1 + months
-    year = sourcedate.year + month / 12
+    year = sourcedate.year + month // 12
     month = month % 12 + 1
     day = min(sourcedate.day, calendar.monthrange(year,month)[1])
     return datetime.date(year, month, day)
