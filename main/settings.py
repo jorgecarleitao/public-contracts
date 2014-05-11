2dda6bfa1c78f05431054edf2884fb97531211b2
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-05-11 07:13:54 +0200

    Added LANGUAGE_CODE to settings to translate url strings.

--- main/settings.py
+++ main/settings.py
@@ -35,6 +35,7 @@ main_directory = os.path.abspath(os.path.dirname(__file__))
 site_directory = os.path.abspath(os.path.dirname(main_directory))
 
 TIME_ZONE = 'Europe/Lisbon'
+LANGUAGE_CODE = 'pt'
 
 DATABASES = {
     'default': {
