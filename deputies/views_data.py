3cc0924942611273be281cef060e79642a7622a8
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-05-21 18:47:27 +0200

    Fixed deputies analysis data view.

--- deputies/views_data.py
+++ deputies/views_data.py
@@ -2,14 +2,13 @@ import json
 
 from django.http import HttpResponse
 
-from .analysis import AnalysisManager
+from .analysis import analysis_manager
 
 
 def deputies_time_distribution_json(request):
-    data = AnalysisManager.get_analysis('deputies_time_distribution')
+    data = analysis_manager.get_analysis('deputies_time_distribution')
 
     for x in data:
         x['value'] = x['count']
 
     return HttpResponse(json.dumps(data), content_type="application/json")
-
