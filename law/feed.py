73163bf6e4b367d2353e33e8ac074d63239f331f
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-06-04 09:55:43 +0200

    Added rss feed to law list and law list of a given type.

---
+++ law/feed.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
+from django.utils import formats
+from django.utils.translation import ugettext as _
+
+from main.domain import SITE_NAME
+
+from .models import Document, Type
+
+
+class LawsFeed(Feed):
+    def title(self, __):
+        return _("Laws in %s" % SITE_NAME)
+
+    def link(self, _):
+        return reverse('laws_list_feed')
+
+    def description(self, __):
+        return _("Latest laws in %s" % SITE_NAME)
+
+    def item_title(self, item):
+        return _('%s of %s - %s' % (item.name(), formats.date_format(item.date), item.creator.name))
+
+    def item_description(self, item):
+        return item.summary
+
+    def items(self, _):
+        return Document.laws.order_by('-date').prefetch_related('type', 'creator')[:200]
+
+
+class TypeDocumentsFeed(Feed):
+    def get_object(self, request, type_id, slug=None):
+        return get_object_or_404(Type, id=type_id)
+
+    def title(self, obj):
+        return "%s in %s" % (obj.name, SITE_NAME)
+
+    def link(self, obj):
+        return obj.get_absolute_url()
+
+    def description(self, obj):
+        return _("Documents \"%s\"" % obj.name)
+
+    def item_title(self, item):
+        return _('%s of %s - %s' % (item.name(), formats.date_format(item.date), item.creator.name))
+
+    def item_description(self, item):
+        return item.summary
+
+    def items(self, obj):
+        return obj.document_set.order_by('-date').prefetch_related('type', 'creator')[:200]
--- law/urls.py
+++ law/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from django.utils.translation import ugettext_lazy as _
 
-from . import views, views_data
+from . import views, views_data, feed
 
 urlpatterns = patterns('',
                        url(r'^%s/%s$' % (_('law'), _('home')), views.home, name='law_home'),
@@ -18,9 +18,16 @@ urlpatterns = patterns('',
                            ),
 
                        url(r'^%s$' % _('law'), views.law_list, name='law_law_list'),
+                       url(r'^%s/rss$' % _('law'), feed.LawsFeed(), name='laws_list_feed'),
+
                        url(r'^%s$' % _('types'), views.types_list, name='law_types_list'),
+
                        url(r'^%s/(\d+)$' % _('type'), views.type_view),
                        url(r'^%s/(\d+)/([-\w]+)$' % _('type'), views.type_view, name='law_type'),
+
+                       url(r'^%s/(\d+)/rss$' % _('type'), feed.TypeDocumentsFeed()),
+                       url(r'^%s/(\d+)/([-\w]+)/rss$' % _('type'), feed.TypeDocumentsFeed(),
+                           name='type_documents_feed'),
 
                        url(r'^%s/(\d+)$' % _('document'), views.law_view, name='law_view'),
                        url(r'^%s/(\d+)/(.*)' % _('document'), views.law_view, name='law_view'),
