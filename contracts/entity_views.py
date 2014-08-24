jorgecarleitao/public-contracts

commit 7c8b73f2287b89270eadaee5d5b471c68fdef18b

Improved page load of landing page of entity.

contracts/entity_views.py
@@ -17,9 +17,12 @@ def main_view(request, entity_id, slug=None):
     entity = get_object_or_404(models.Entity.objects.select_related('data'),
                                pk=entity_id)
 
-    contracts = list(entity.contract_set.prefetch_related("contracted",
-                                                          "contractors",
-                                                          "category")[:5])
+    ids = entity.get_contracts_ids()
+    contracts = models.Contract.objects.filter(id__in=ids['made'] + ids['set'])
+
+    # optimization
+    contracts = contracts.prefetch_related("contracted", "contractors")[:5]
+
     categories = models.Category.objects.filter(pk__in=[c.category_id
                                                         for c in contracts])
     clients_hiring, hired_clients = entity.main_costumers()
@@ -27,6 +30,8 @@ def main_view(request, entity_id, slug=None):
     context = {'navigation_tab': 'entities',
                'entity': entity,
                'tab': 'summary',
+               'contracts_made_count': len(ids['made']),
+               'contracts_received_count': len(ids['set']),
                'last_contracts': contracts,
                'last_categories': categories,
                'hired_clients': hired_clients,
@@ -38,9 +43,11 @@ def main_view(request, entity_id, slug=None):
 def contracts(request, entity_id):
     entity = get_object_or_404(models.Entity, pk=entity_id)
 
-    contracts = indexes.ContractIndex.objects.filter(id__in=entity.get_contracts_ids())\
-        .search_filter(id__in=entity.get_contracts_ids())\
-        .prefetch_related("contracted", "contractors", "category")
+    contracts = indexes.ContractIndex.objects\
+        .filter(id__in=entity.get_contracts_ids())\
+        .search_filter(id__in=entity.get_contracts_ids())
+
+    contracts = contracts.prefetch_related("contracted", "contractors")
 
     context = {'navigation_tab': 'entities',
                'entity': entity,

contracts/models.py
@@ -89,40 +89,14 @@ class Entity(models.Model):
 
     country = models.ForeignKey('Country', null=True)
 
+    def __unicode__(self):
+        return self.name
+
     def total_earned(self):
         return self.data.total_earned
 
     def total_expended(self):
         return self.data.total_expended
-
-    def get_contracts_ids(self, flush_cache=False):
-        """
-        Retrieves a list of all contracts' ids the entity participated in.
-        The result is cached for 1 month because it is updated using cron.
-        """
-        cache_name = __name__ + '>contracts_ids' + '>%s' % self.id
-        result = cache.get(cache_name)
-        if result is None or flush_cache:
-            # retrieves the list. This is an expensive query.
-            result = \
-                list(self.contract_set.all().values_list('id', flat=True)) + \
-                list(self.contracts_made.all().values_list('id', flat=True))
-            cache.set(cache_name, result, 60*60*24*30)
-        return result
-
-    def last_contracts(self, slice_value=None):
-        query = Contract.objects.filter(id__in=self.get_contracts_ids())
-
-        if slice_value is None:
-            return query
-
-        try:
-            return query[:slice_value]
-        except IndexError:
-            return query
-
-    def __unicode__(self):
-        return self.name
 
     def get_base_url(self):
         return 'http://www.base.gov.pt/base2/html/pesquisas/entidades.shtml#%s' \
@@ -130,6 +104,26 @@ class Entity(models.Model):
 
     def get_absolute_url(self):
         return reverse('entity', args=(self.id, slugify(self.name)))
+
+    def get_contracts_ids(self, flush_cache=False):
+        """
+        Lists all contracts ids the entity participated in.
+        The result is cached for 1 month since it is updated using cron.
+
+        Returns a dictionary with keys 'made' and 'set' with the list of
+        ids of contracts_made and contract_set respectively.
+        """
+        cache_name = __name__ + '>contracts_ids' + '>%s' % self.id
+        result = cache.get(cache_name)
+        if result is None or flush_cache:
+            # retrieves the lists.
+            result = {'made': list(self.contracts_made.all()
+                                   .values_list('id', flat=True)),
+                      'set': list(self.contract_set.all()
+                                  .values_list('id', flat=True))
+                      }
+            cache.set(cache_name, result, 60*60*24*30)
+        return result
 
     def compute_data(self):
         """

contracts/templates/contracts/entity_view/tab_contracts/main.html
@@ -1,6 +1,8 @@
 {% extends "contracts/entity_view/main.html" %}
 {% load i18n %}
 {% load contracts.humanize %}
+
+{% block title %}{{ entity.name }} - {% trans "Contracts" %}}{% endblock %}
 {% block entity_content %}
     {% include "contracts/contract_list/contract_list_template.html"%}
 {% endblock %}

contracts/templates/contracts/entity_view/tab_data/main.html
@@ -2,91 +2,89 @@
 {% load i18n %}
 {% load contracts.humanize %}
 {% block entity_content %}
-    {% with count_contracts=entity.contracts_made.count count_contracts_received=entity.contract_set.count %}
-        <div class="row">
-            <div class="col-md-6">
-                <div class="panel panel-default">
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <span class="glyphicon glyphicon-bookmark"></span>
-                            <span><strong>{% trans "Fiscal Number" %}:</strong></span>
-                            <span>{{ entity.nif }}</span>
-                        </li>
-                        <li class="list-group-item">
-                            <span class="glyphicon glyphicon-link"></span>
-                            <span><strong>{% trans "Official record" %}:</strong></span>
-                            <a href="{{ entity.get_base_url }}">
-                                {{ entity.base_id }}</a>
-                        </li>
-                        <li class="list-group-item">
-                            <span class="glyphicon glyphicon-upload"></span>
-                            <span><strong>{% trans "Earned" %}:</strong></span>
-                            <span>{{ entity.total_earned|intword }} &euro;</span>
-                            <span>({{ count_contracts_received }} {% trans "contracts" %})</span>
-                            {% if count_contracts_received %}
-                                {% url 'entity_contracts_received_time_series' entity.id as the_url %}
-                                {% include "contracts/entity_view/tab_data/graph.html" with url=the_url %}
-                            {% endif %}
-                        </li>
-                        <li class="list-group-item">
-                            <span class="glyphicon glyphicon-download"></span>
-                            <span><strong>{% trans "Expended" %}:</strong></span>
-                            <span>{{ entity.total_expended|intword }} &euro;</span>
-                            <span>({{ count_contracts }} {% trans "contracts" %})</span>
-                            {% if count_contracts %}
-                                {% url 'entity_contracts_made_time_series' entity.id as the_url %}
-                                {% include "contracts/entity_view/tab_data/graph.html" with url=the_url %}
-                            {% endif %}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="panel panel-default">
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <span class="glyphicon glyphicon-briefcase"></span>
-                            <span><strong>
-                                {% trans "Last contracts" %}:</strong></span>
-                            {% include "contracts/contracts_minor_list.html" with contracts=last_contracts %}
-                            <p><a href="{% url "entity_contracts" entity.id %}">{% trans "Full list" %}</a></p>
-                        </li>
-                        {% if clients_hiring %}
-                            <li class="list-group-item">
-                                <span class="glyphicon glyphicon-briefcase"></span>
-                                <span class="glyphicon glyphicon-upload"></span>
-                            <span><strong>
-                                {% trans "Main clients (hired)" %}:</strong></span>
-                                <ul class="list-group">
-                                    {% for client in clients_hiring %}
-                                        <li class="list-group-item">
-                                            <a href="
-                                            {{ client.get_absolute_url }}">{{client.name}}</a>:
-                                            {% include "inline_contracts_number.html" with contracts_count=client.total_contracts %}
-                                        </li>
-                                    {% endfor %}
-                                </ul>
-                            </li>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-bookmark"></span>
+                        <span><strong>{% trans "Fiscal Number" %}:</strong></span>
+                        <span>{{ entity.nif }}</span>
+                    </li>
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-link"></span>
+                        <span><strong>{% trans "Official record" %}:</strong></span>
+                        <a href="{{ entity.get_base_url }}">
+                            {{ entity.base_id }}</a>
+                    </li>
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-upload"></span>
+                        <span><strong>{% trans "Earned" %}:</strong></span>
+                        <span>{{ entity.total_earned|intword }} &euro;</span>
+                        <span>({% include "inline_contracts_number.html" with contracts_count=contracts_received_count%})</span>
+                        {% if contracts_received_count %}
+                            {% url 'entity_contracts_received_time_series' entity.id as the_url %}
+                            {% include "contracts/entity_view/tab_data/graph.html" with url=the_url %}
                         {% endif %}
-                        {% if hired_clients %}
-                            <li class="list-group-item">
-                                <span class="glyphicon glyphicon-briefcase"></span>
-                                <span class="glyphicon glyphicon-download"></span>
-                                <span><strong>{% trans "Main clients (were hired)" %}:</strong></span>
-                                <ul class="list-group">
-                                    {% for client in hired_clients %}
-                                        <li class="list-group-item">
-                                            <a href="
-                                            {{ client.get_absolute_url }}">{{client.name}}</a>:
-                                            {% include "inline_contracts_number.html" with contracts_count=client.total_contracts %}
-                                        </li>
-                                    {% endfor %}
-                                </ul>
-                            </li>
+                    </li>
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-download"></span>
+                        <span><strong>{% trans "Expended" %}:</strong></span>
+                        <span>{{ entity.total_expended|intword }} &euro;</span>
+                        <span>({{ contracts_made_count }} {% trans "contracts" %})</span>
+                        {% if contracts_made_count %}
+                            {% url 'entity_contracts_made_time_series' entity.id as the_url %}
+                            {% include "contracts/entity_view/tab_data/graph.html" with url=the_url %}
                         {% endif %}
-                    </ul>
-                </div>
+                    </li>
+                </ul>
             </div>
         </div>
-    {% endwith %}
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <span class="glyphicon glyphicon-briefcase"></span>
+                            <span><strong>
+                                {% trans "Last contracts" %}:</strong></span>
+                        {% include "contracts/contracts_minor_list.html" with contracts=last_contracts %}
+                        <p><a href="{% url "entity_contracts" entity.id %}">{% trans "Full list" %}</a></p>
+                    </li>
+                    {% if clients_hiring %}
+                        <li class="list-group-item">
+                            <span class="glyphicon glyphicon-briefcase"></span>
+                            <span class="glyphicon glyphicon-upload"></span>
+                            <span><strong>
+                                {% trans "Main clients (hired)" %}:</strong></span>
+                            <ul class="list-group">
+                                {% for client in clients_hiring %}
+                                    <li class="list-group-item">
+                                        <a href="
+                                            {{ client.get_absolute_url }}">{{client.name}}</a>:
+                                        {% include "inline_contracts_number.html" with contracts_count=client.total_contracts %}
+                                    </li>
+                                {% endfor %}
+                            </ul>
+                        </li>
+                    {% endif %}
+                    {% if hired_clients %}
+                        <li class="list-group-item">
+                            <span class="glyphicon glyphicon-briefcase"></span>
+                            <span class="glyphicon glyphicon-download"></span>
+                            <span><strong>{% trans "Main clients (were hired)" %}:</strong></span>
+                            <ul class="list-group">
+                                {% for client in hired_clients %}
+                                    <li class="list-group-item">
+                                        <a href="
+                                            {{ client.get_absolute_url }}">{{client.name}}</a>:
+                                        {% include "inline_contracts_number.html" with contracts_count=client.total_contracts %}
+                                    </li>
+                                {% endfor %}
+                            </ul>
+                        </li>
+                    {% endif %}
+                </ul>
+            </div>
+        </div>
+    </div>
 {% endblock %}

commit ac68dd000bc4f1066ca78bc0d51c12118a95da79

Fixed 2 errors introduced in 7c8b73f228.

contracts/entity_views.py
@@ -44,8 +44,8 @@ def contracts(request, entity_id):
     entity = get_object_or_404(models.Entity, pk=entity_id)
 
     contracts = indexes.ContractIndex.objects\
-        .filter(id__in=entity.get_contracts_ids())\
-        .search_filter(id__in=entity.get_contracts_ids())
+        .filter(id__in=entity.get_all_contracts_ids())\
+        .search_filter(id__in=entity.get_all_contracts_ids())
 
     contracts = contracts.prefetch_related("contracted", "contractors")
 

contracts/feed.py
@@ -116,7 +116,7 @@ class EntityContractsFeed(Feed):
         return result[:-2]
 
     def items(self, obj):
-        return Contract.objects.filter(id__in=obj.get_contracts_ids())[:200]
+        return Contract.objects.filter(id__in=obj.get_all_contracts_ids())[:200]
 
 
 class EntityTendersFeed(EntityContractsFeed):

contracts/models.py
@@ -125,6 +125,10 @@ class Entity(models.Model):
             cache.set(cache_name, result, 60*60*24*30)
         return result
 
+    def get_all_contracts_ids(self):
+        ids = self.get_contracts_ids()
+        return ids['made'] + ids['set']
+
     def compute_data(self):
         """
         Computes the data of this entity from the existing relations.
@@ -144,7 +148,7 @@ class Entity(models.Model):
         self.data.total_expended = aggregate['price__sum'] or 0
         self.data.save()
 
-        # update list of contracts
+        # update list of contracts on cache
         self.get_contracts_ids(flush_cache=True)
 
     def main_costumers(self):