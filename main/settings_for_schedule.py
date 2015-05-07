b26bf9dc894c27b0016e01c2d0b1be184ad477f2
Jorge C. Leitão <jorgecarleitao@gmail.com>
2015-05-07 07:51:04 +0200

    Simplified settings_for_schedule.py

--- main/settings_for_schedule.py
+++ main/settings_for_schedule.py
@@ -1,9 +1,11 @@
 """
 Settings file for the scheduler. Requires a `settings_live` with private
 information of the server.
+
+Imports everything from `settings.py`, and them overwrites some of the settings.
 """
+from .settings import *
 from . import settings_live
-from . import domain
 
 INSTALLED_APPS = (
     'deputies',
@@ -13,60 +15,15 @@ INSTALLED_APPS = (
     'django_rq',
 )
 
-
-DATABASES = settings_live.DATABASES
-
+# django_rq related
 RQ_QUEUES = settings_live.RQ_QUEUES
-
-SECRET_KEY = settings_live.SECRET_KEY
-
-CACHES = settings_live.CACHES
-
-ADMINS = settings_live.ADMINS
-
-TIME_ZONE = 'Europe/Lisbon'
-
-if hasattr(settings_live, 'EMAIL_HOST'):
-    EMAIL_HOST = settings_live.EMAIL_HOST
-    EMAIL_HOST_USER = settings_live.EMAIL_HOST_USER
-    EMAIL_HOST_PASSWORD = settings_live.EMAIL_HOST_PASSWORD
-    DEFAULT_FROM_EMAIL = settings_live.DEFAULT_FROM_EMAIL
-    SERVER_EMAIL = settings_live.SERVER_EMAIL
-    EMAIL_SUBJECT_PREFIX = '[%s] ' % domain.SITE_DOMAIN
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
-        },
-    },
-    'handlers': {
-        # INFO to console.
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-        # errors to admins
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        # This is the "catch all" logger
-        '': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
 
 # ignore requests logging
 import logging
 logging.getLogger("requests").setLevel(logging.WARNING)
+
+# use DEBUG for pt_law_downloader so if any publication fails to download,
+# we have a log of its number.
+l = logging.getLogger('pt_law_downloader')
+l.setLevel(logging.DEBUG)
+l.addHandler(logging.StreamHandler())
